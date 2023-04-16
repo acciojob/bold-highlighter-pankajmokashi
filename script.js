98d2d0e7b32a62e3deb6f5fbf84c069692d877d0
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-16 14:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let st = document.querySelectorAll("strong");
+	for (let i = 0; i < st.length; i++) {
+		  st[i].style.color = "green";
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let st = document.querySelectorAll("strong");
+	for (let i = 0; i < st.length; i++) {
+		  st[i].style.color = "black";
+	}
     
 }
